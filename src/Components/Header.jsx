63be225b0eedae8d553eec1d06df4e086f1d27f0
DKofJustice <alexbenta20@gmail.com>
2023-01-21 00:07:22 +0200

    Styling and working on the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,27 @@ import SearchIcon from './../Assets/material-symbols_search-rounded.png';
 import ShopIcon from './../Assets/material-symbols_shopping-bag-outline.png';
 import UserIcon from './../Assets/mdi_user-circle-outline.png';
 import HamButton from './../Assets/mdi_hamburger-menu.svg';
+import { useState } from 'react';
 
 export default function Header() {
+
+    const [header, setHeader] = useState(false);
+    const [headerPos, setHeaderPos] = useState('');
+
+    const toggleHeader = () => {
+        if (header === false) {
+            setHeaderPos('header-showing');
+            setHeader(true);
+        } else {
+            setHeaderPos('');
+            setHeader(false);
+        }
+    };
+
   return (
-    <header>
+    <>
+    <img src={HamButton} alt="ham-button" onClick={toggleHeader} />
+    <header className={headerPos}>
         <div className='header-content'>
           <div className='logo-content'>
               <img src={VictusLogoGreen} alt="victus-logo-green" />
@@ -27,5 +44,6 @@ export default function Header() {
           </nav>
         </div>
     </header>
+    </>
   )
 }
