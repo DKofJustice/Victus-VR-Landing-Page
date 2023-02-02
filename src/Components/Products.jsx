4921b76426d4e0e36e85d171076459e100c366e6
DKofJustice <alexbenta20@gmail.com>
2023-02-02 21:57:45 +0200

    Completed the Reviews section

---
+++ src/Components/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <div className='products'>
+        <div className='products-content'>
+            <h2>Check our other products</h2>
+
+            <div className='product-list'>
+                </>
+            </div>
+        </div>
+    </div>
+  )
+}
